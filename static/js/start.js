6d68ced3930901838a73cab806f855f3887c7fff
Peter Munk <info@peter-munk.de>
2021-02-02 22:45:08 +0100

    More heroku adaptions

--- static/js/start.js
+++ static/js/start.js
@@ -4,7 +4,7 @@ var socket = io.connect('https://activity-backend.herokuapp.com/');
 function createRoom() {
     var roomName = $(" #roomName ").val();
     console.log("Created room " + roomName);    
-    window.open("http://localhost:5000/"+roomName,"_self")
+    window.open("https://activity-backend.herokuapp.com/"+roomName,"_self")
 }
 
 $(" #btn_room ").click(function() {createRoom();});
